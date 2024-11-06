eb62f89a3566b10474f813297147dd992a3db6a8
RichardJesse <jesse.richard@strathmore.edu>
2024-11-06 16:57:06 +0300

    ''

--- autopush.php
+++ autopush.php
@@ -231,7 +231,7 @@ class AutoPush
      */
     private function log($message)
     {
-      $signature = $this->style("Autopush", self::f)
+      $signature = $this->style("Autopush", self::FG_CYAN, )
 
         echo "AutoPush: $message \n\n";
         return $this;
